2c419308ccb08a9643430f0c0838e411bfb6d32b
opsaaaaa <sean@ferney.org>
2023-07-28 15:18:56 -0600

    tweeks

--- src/view/menu.tsx
+++ src/view/menu.tsx
@@ -7,14 +7,18 @@ import type {ViewProps} from './props'
 export function MenuView({setRoute}: ViewProps) {
 
   return (
-    <div style={{
+    <div
+    style={{
       display: 'grid',
       justifyContent: 'center',
+      alignContent: 'center',
+      height: '100%',
+      padding: '3rem',
       gap: '3rem',
-      margin: '3rem',
-    }}>
+    }}
+    >
       <h1 className="m-auto">Minituar Maze</h1>
-      <p style={{textAlign: 'center'}}>
+      <p className="m-auto" style={{textAlign: 'center', maxWidth: '30em'}}>
         The goal of the <strong>Minotaur</strong> is to capture the players.
         The goal of the remaining players is to be the first to <strong>escape to the flag</strong>.
         Good luck, and have fun!
