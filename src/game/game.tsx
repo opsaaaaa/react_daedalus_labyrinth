e7ccda0de3cb2877cc0655b4c0d35d2e4db51d9f
opsaaaaa <sean@ferney.org>
2023-07-27 19:42:59 -0600

    adjusting board style; prep for game.state; fix bug with conflicting player death/win

--- src/game/game.tsx
+++ src/game/game.tsx
@@ -6,19 +6,29 @@ import {create_actor_list} from "../game/actor"
 
 // TODO: implement some kind of settings for board size.
 
+export enum GAME_STATE {
+  PLAY_MOVE,
+  PLAY_SLIDE,
+  GAME_OVER,
+  PAUSE,
+}
+
 export class Game {
   board: Board;
   insert_btns: InsertBtns;
 
   actors: Actor[];
   selected_actor: Actor | undefined;
+  turn_actor: Actor;
 
   goal: Tile;
+
+  state: GAME_STATE;
 
   constructor(w:number,h:number) {
     this.board = new Board(w,h)
     this.insert_btns = new InsertBtns(w,h)
-    
+
     this.actors = create_actor_list([
       this.board.cell(0,0),
       this.board.cell(w - 1, 0),
@@ -27,10 +37,13 @@ export class Game {
     ])
 
     this.selected_actor = undefined
+    this.turn_actor = this.actors[0]
 
     this.goal = this.board.cell( Math.floor((w - 1)/2), Math.floor((h - 1)/2) )
 
     this.build_actor_moves()
+
+    this.state = GAME_STATE.PLAY_SLIDE
   }
 
   build_actor_moves(): void {
@@ -59,8 +72,8 @@ export class Game {
     this.selected_actor.tile = m
     this.selected_actor.moves = []
 
-    this.check_win(this.selected_actor)
-    this.check_revive(this.selected_actor)
+    this.check_win(this.selected_actor) ||
+    this.check_revive(this.selected_actor) &&
     this.check_death(this.selected_actor)
 
     this.selected_actor = undefined
@@ -72,32 +85,36 @@ export class Game {
     this.build_actor_moves()
   }
 
-  check_win(a: Actor): void {
-    if (a.is_minotaur()) {return}
-    if (this.goal.id !== a.tile.id) { return }
+  check_win(a: Actor): boolean {
+    if (a.is_minotaur()) {return false }
+    if (this.goal.id !== a.tile.id) { return false }
     a.win()
+    return true
   }
 
-  check_death(a: Actor): void {
+  check_death(a: Actor): boolean {
     if(a.is_minotaur()) {
       const kill = this.actors.find(k=>(a.tile.id === k.tile.id && !k.is_minotaur() && k.is_alive()))
       console.log({kill, a, m: 'kill'})
-      if (!kill) {return}
+      if (!kill) {return false}
       kill.die()
+      return true
     } else {
       const mino = this.get_minotar()
-      if(mino.tile.id !== a.tile.id) { return }
+      if(mino.tile.id !== a.tile.id) { return false }
       a.die()
+      return true
     }
   }
 
-  check_revive(a: Actor): void {
-    if (a.is_minotaur()) {return}
+  check_revive(a: Actor): boolean {
+    if (a.is_minotaur()) {return false}
     const r = this.actors.find(r=>(a.tile.id === r.tile.id && r.is_dead()))
-    if (!r) {return}
+    if (!r) {return false}
 
     r.revive()
     this.build_single_actor_moves(r)
+    return true
   }
 
   get_minotar(): Actor {
